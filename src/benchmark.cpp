2a6e6f80bcae1d1fe8355fae69fa7b716510e2df
Kevin Yu <kevinoyu@gmail.com>
2023-11-21 16:35:42 -0600

    add benchmarks

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -24,6 +24,7 @@ std::vector<unsigned long> create_data() {
                             for(unsigned long o = n >> 1; o > 0; o >>= 1)
                                 v.push_back(i | j | k | l | m | n | o);  
 
+    // convert packed 52bit into internal 64bit
     for(auto& x: v) {
         auto tmp = 0UL;
         for(int i = 0; i < 4; ++i) {
@@ -45,21 +46,49 @@ static void BM_evaluate(benchmark::State& state) {
         }
         auto cards = data[i++];
         benchmark::DoNotOptimize(evaluate_hand(cards));
+        benchmark::ClobberMemory();
+    }
+}
+
+static void BM_get(benchmark::State& state) {
+    int i = 0;
+    auto data = create_data();
+    for (auto _ : state) {
+        if(i >= COMBOS) {
+            i = 0;
+        }
+        auto cards = data[i++];
+        benchmark::DoNotOptimize(cards);
+        benchmark::ClobberMemory();
     }
 }
 
 static void BM_get_rand(benchmark::State& state) {
-    int i = 0;
-    auto data = create_data();
+    std::mt19937 g{std::random_device{}()};
+    std::uniform_int_distribution<size_t> d{0,COMBOS};
 
+    std::vector<unsigned long> data(COMBOS, 0);
+    
     for(auto _ : state) {
-        if(i >= COMBOS) {
-            i = 0;
-        }
-        benchmark::DoNotOptimize(data[i++]);
+        auto a = data[d(g)];
+        benchmark::DoNotOptimize(a);
+        benchmark::ClobberMemory();
     }
 }
 
+static void BM_rand(benchmark::State& state) {
+    std::mt19937 g{std::random_device{}()};
+    std::uniform_int_distribution<size_t> d{0,COMBOS};
+
+    for(auto _ : state) {
+        auto a = d(g);
+        benchmark::DoNotOptimize(a);
+        benchmark::ClobberMemory();
+    }
+}
+    
 BENCHMARK(BM_evaluate);
+BENCHMARK(BM_get);
 BENCHMARK(BM_get_rand);
+BENCHMARK(BM_rand);
 BENCHMARK_MAIN();
